bc878840f527933a455c0e6517d6a53d510076a5
Débora Souza <148487575+deborasouza01@users.noreply.github.com>
2025-08-03 16:55:59 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const button = document.getElementById("whatsapp-button");
-    
-    setInterval(() => {
-        button.classList.toggle("animate-bounce");
-    }, 2000);
-});
